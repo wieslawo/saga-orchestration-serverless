ca0d7fdac1bbf022af98536d91ce63efe4c7fccd
wieslaw <wieslawo@poczta.onet.pl>
2023-01-05 23:11:01 +0100

    test2

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    # modify the values for the storage account ytry
+    # modify the values for the storage account yt
     storage_account_name = "storagewieslawo1"
     container_name       = "testingterraform"
     key                  = "terraform.tfstate"
